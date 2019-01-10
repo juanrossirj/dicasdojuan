a17eef9089bbcbd20f979f233aa42ff52f0dd398
juanrossirj <juan.ignacio.rossi@gmail.com>
2019-01-10 21:45:34 -0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"  <li><a href='index.html'>Início</a></li>" +
+"  <li><a href='00_poupar.html'>Dicas Renda Fixa</a></li>" +
+"  <li><a href='calculador.html'>Cálculos</a></li>" +
+"</ul>" +
+"<br>" +
+"<br>";
